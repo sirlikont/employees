de2c2ba0e264043e8fe5213d60e295494e01ede5
Sirli Kont <sirli.kont@pipedrive.com>
2022-08-02 14:20:54 +0300

    joi

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("koa-bodyparser");
 app.use(bodyParser());
 
 const router = require('./lib/routes/employees-router');
-app.use(router.routes());
+app.use(router.middleware());
 
 app.listen(3000, () =>
    console.log('Server is running at http://localhost:3000')
